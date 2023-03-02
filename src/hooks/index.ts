9d55fa1dd379cbea3ad176e52976ab7916a83a6d
Evandro Souza Pereira <evandro.souza.pereira@gmail.com>
2023-03-02 09:32:56 -0300

    fix hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,25 +2,25 @@ import { useLayoutEffect, useState } from "react"
 const baseUrl = import.meta.env.VITE_API_BASE_URL
 
 export const useWindowSize = (): boolean => {
-    const [windowSize, setWindowSize] = useState(false)
-    const updateWindowSize = () => {
-        window.innerWidth <= 480 ? setWindowSize(false) : setWindowSize(true)
-    }
-    useLayoutEffect(() => {
-        window.addEventListener('resize', updateWindowSize);
-        updateWindowSize();
-        return () => window.removeEventListener('resize', updateWindowSize);
-    },[])
-    return windowSize
+  const [windowSize, setWindowSize] = useState(false)
+  const updateWindowSize = () => {
+    window.innerWidth <= 480 ? setWindowSize(false) : setWindowSize(true)
+  }
+  useLayoutEffect(() => {
+    window.addEventListener('resize', updateWindowSize);
+    updateWindowSize();
+    return () => window.removeEventListener('resize', updateWindowSize);
+  }, [])
+  return windowSize
 }
 
-export const sendDataDb = (data : any) => {
-    fetch(baseUrl, {
-        method: "POST",
-        body: data
-      }).then((response) => {
-        return response.json()
-      }).then((response) => {
-        console.log(response)
-      })
+export const sendDataDb = (data: any) => {
+  fetch("https://localhost", {
+    method: "POST",
+    body: data
+  }).then((response) => {
+    return response.json()
+  }).then((response) => {
+    console.log(response)
+  })
 }
